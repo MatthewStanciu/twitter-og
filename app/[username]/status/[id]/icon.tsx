a83b41a764413eef9a46aaca6b7b1ca7e650a833
Matthew Stanciu <mattbstanciu@gmail.com>
2023-07-02 17:23:22 -0700

    Generate icon

---
+++ app/[username]/status/[id]/icon.tsx
@@ -0,0 +1,35 @@
+import { fetchTweet } from "@/lib/fetchTweet";
+import { ImageResponse } from "next/server";
+
+export const runtime = "edge";
+
+export const size = {
+  width: 32,
+  height: 32,
+};
+export const contentType = "image/png";
+
+export default async function Icon({
+  params,
+}: {
+  params: { username: string; id: string };
+}) {
+  console.log({ params });
+  const { username, id } = params;
+  const { user } = await fetchTweet(id);
+  const avatarUrl = user.profile_image_url_https;
+
+  return new ImageResponse(
+    (
+      // eslint-disable-next-line @next/next/no-img-element
+      <img
+        style={{ borderRadius: "50%" }}
+        alt={`Twitter user ${username}'s avatar`}
+        src={avatarUrl}
+      />
+    ),
+    {
+      ...size,
+    }
+  );
+}
